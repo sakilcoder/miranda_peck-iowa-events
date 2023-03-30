e44b44a8a32f0a710cdb04cbf2cb5cb4f66ae123
Sakil Ahmed <sakil@sakil.local>
2023-03-30 08:23:45 +0600

    e

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
 const map = L.map('map'); //.setView([48.415, -109.733], 8)
-
+map.options.minZoom=7;
+map.options.maxZoom=12;
 const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
