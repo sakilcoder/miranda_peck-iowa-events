a3ec060873c0bee230e7b674527451d44a41b6ec
Sakil Ahmed <sakilcoder@gmail.com>
2023-04-06 22:34:58 +0600

    region 12 position changed

--- assets/data/iowa-region-point.js
+++ assets/data/iowa-region-point.js
@@ -6,7 +6,7 @@ let points = {
 { "type": "Feature", "properties": { "Region": "Region 1", "ORIG_FID": 0 }, "geometry": { "type": "Point", "coordinates": [ -95.628176753192989, 42.967273505312782 ] } },
 { "type": "Feature", "properties": { "Region": "Region 10", "ORIG_FID": 1 }, "geometry": { "type": "Point", "coordinates": [ -91.631177806870369, 41.917623686266666 ] } },
 { "type": "Feature", "properties": { "Region": "Region 11", "ORIG_FID": 2 }, "geometry": { "type": "Point", "coordinates": [ -90.568149548543374, 41.794607005804266 ] } },
-{ "type": "Feature", "properties": { "Region": "Region 12", "ORIG_FID": 3 }, "geometry": { "type": "Point", "coordinates": [ -93.739390736284946, 41.551251850998938 ] } },
+{ "type": "Feature", "properties": { "Region": "Region 12", "ORIG_FID": 3 }, "geometry": { "type": "Point", "coordinates": [ -93.8036, 41.7620 ] } },
 { "type": "Feature", "properties": { "Region": "Region 2", "ORIG_FID": 4 }, "geometry": { "type": "Point", "coordinates": [ -94.214771319809969, 43.031724648411306 ] } },
 { "type": "Feature", "properties": { "Region": "Region 3", "ORIG_FID": 5 }, "geometry": { "type": "Point", "coordinates": [ -91.860790923037669, 42.996980528227397 ] } },
 { "type": "Feature", "properties": { "Region": "Region 4", "ORIG_FID": 6 }, "geometry": { "type": "Point", "coordinates": [ -95.36669289407871, 42.12929016688814 ] } },
